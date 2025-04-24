34466169acd966fa12717d798cd0b53a80e32b3a
nmintzer-oswego <nmintzer@oswego.edu>
2025-04-23 22:37:28 -0400

    Frontend Error Handling

--- Controller/bus-api.py
+++ Controller/bus-api.py
@@ -315,4 +315,4 @@ def alerts():
 if __name__ == '__main__':
     # Server configuration
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
